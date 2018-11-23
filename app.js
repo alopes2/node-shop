7d5cff864615d489a87406657d7a85263d0c0927
andre <andrevitorlopes@gmail.com>
2018-11-23 09:46:41 -0200

    Adding catch all route

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found!</h1>')
+})
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
