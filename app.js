84e4136a86056610f5c9189d339991de9a443c16
andre <andrevitorlopes@gmail.com>
2018-11-23 09:40:38 -0200

    Fixing body parser

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 const app = express();
 
