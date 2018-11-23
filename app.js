e72585ec2537ba37780ced12a43a686acb42221b
andre <andrevitorlopes@gmail.com>
2018-11-23 09:43:39 -0200

    Adding shop routes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
