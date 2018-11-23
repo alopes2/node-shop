732118d5b41e2ec85da22bbbf0a6637a3315d59b
andre <andrevitorlopes@gmail.com>
2018-11-23 09:42:37 -0200

    Adding admin routes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
 
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
 
-const app = express();
+app.use(adminRoutes);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
