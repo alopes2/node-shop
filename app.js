078bbdb5a86a0ea5fd17ea1a79ad96c326fac5cb
alopes2 <andre.lopes@exxonmobil.com>
2018-12-04 16:19:57 -0200

    adding ejs view engine

--- app.js
+++ app.js
@@ -2,20 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', { pageTitle: 'Page not found!', path: 'not-found'});
 });
 
 app.listen(3000, () => {
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,16 @@ const express = require('express');
 const path = require('path');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     // res.sendFile(path.join(__dirname, '..' ,'views', 'shop.html'));
-    res.sendFile(path.join(rootDir ,'views', 'shop.html'));
+    console.log(adminData.products);
+    const products = adminData.products;
+    // res.sendFile(path.join(rootDir ,'views', 'shop.html'));
+    res.render('shop', { prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0});
 });
 
 module.exports = router;
