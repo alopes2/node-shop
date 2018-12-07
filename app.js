b665992dfc5a54bac940411e854032da9484c7b0
alopes2 <andre.lopes@exxonmobil.com>
2018-12-07 10:43:28 -0200

    adding controller for errors

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const errorController = require('./controllers/error.js');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,10 +19,7 @@ app.use(shopRoutes);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', { pageTitle: 'Page not found!', path: 'not-found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
