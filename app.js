f9452a9bc09e3d20d27b95f3c24485ab253dbbeb
alopes2 <andre.lopes@exxonmobil.com>
2019-01-07 14:46:02 -0200

    creating and realting cart to other models

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -38,9 +40,15 @@ app.use(errorController.get404);
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 sequelize
-	// .sync({ force: true })
-	.sync()
+	.sync({ force: true })
+	// .sync()
 	.then(result => {
 		// console.log(result);
 		return User.findById(1);
